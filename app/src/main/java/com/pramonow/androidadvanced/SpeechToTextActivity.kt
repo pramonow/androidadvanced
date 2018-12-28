7ae3518b3a8ee603c864da1570f8e60c6a5e20c5
Pramono <pramono.winata@tokopedia.com>
2018-12-29 00:07:23 +0700

    Added speech to text capability

---
+++ app/src/main/java/com/pramonow/androidadvanced/SpeechToTextActivity.kt
@@ -0,0 +1,84 @@
+package com.pramonow.androidadvanced
+
+import java.util.ArrayList
+import java.util.Locale
+
+import android.R.menu
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+
+class SpeechToTextActivity : AppCompatActivity() {
+
+    private lateinit var txtSpeechInput: TextView
+    private lateinit var btnSpeak: ImageButton
+    private val REQ_CODE_SPEECH_INPUT = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speech_text)
+
+        txtSpeechInput = findViewById(R.id.txtSpeechInput)
+        btnSpeak = findViewById(R.id.btnSpeak)
+
+        // hide the action bar
+        //actionBar!!.hide()
+
+        btnSpeak!!.setOnClickListener { promptSpeechInput() }
+
+    }
+
+    /**
+     * Showing google speech input dialog
+     */
+    private fun promptSpeechInput() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        intent.putExtra(
+            RecognizerIntent.EXTRA_PROMPT,
+            getString(R.string.speech_prompt)
+        )
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT)
+        } catch (a: ActivityNotFoundException) {
+            Toast.makeText(
+                applicationContext,
+                getString(R.string.speech_not_supported),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+    }
+
+    /**
+     * Receiving speech input
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_CODE_SPEECH_INPUT -> {
+                if (resultCode == Activity.RESULT_OK && null != data) {
+
+                    val result = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    txtSpeechInput!!.text = result[0]
+                }
+            }
+        }
+    }
+
+
+}
