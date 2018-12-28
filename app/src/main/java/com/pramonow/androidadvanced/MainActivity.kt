ea18d06910737567ac35bfc71ac5c0bff660389a
Pramono <pramono.winata@tokopedia.com>
2018-12-28 23:43:28 +0700

    Initial commit

---
+++ app/src/main/java/com/pramonow/androidadvanced/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.pramonow.androidadvanced
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Map
+        //FCM
+        //Nearby
+        //Voice
+        //Mobile Vision
+        //Speech to text
+        //annotation
+    }
+}
